1789536407e632ec86608a57867e540513b32478
Pravin Mishra <pravinmishra@Koos-Laptop.local>
2023-12-01 15:21:33 +0530

    Refactor main function

--- AddressService.kt
+++ AddressService.kt
@@ -2,4 +2,7 @@ class AddressService(private val addressRepository: AddressRepository) {
     fun addAddress(addressDTO: AddressDTO):String{
       return  addressRepository.addAddress(addressDTO)
     }
+    fun searchAddressByName(namePrefix:String):List<AddressDTO>{
+        return addressRepository.searchAddressByName(namePrefix)
+    }
 }
--- Main.kt
+++ Main.kt
@@ -6,34 +6,10 @@ fun main(){
     val addressService = AddressService(trieAddressRepository)
 
     while (true) {
-        println("\n1. Add Address Detail")
-        println("2. Search by Name Prefix")
-        println("3. Search by Number Prefix")
-        println("4. Exit")
-        print("Choose an option: ")
-
+         printOptionMessage()
         when (scanner.nextInt()) {
             1 -> {
-                print("Enter First Name: ")
-                val firstName = scanner.next()
-
-                print("Enter Last Name: ")
-                val lastName = scanner.next()
-                scanner.nextLine()
-                print("Enter Address: ")
-                val address = scanner.nextLine()
-
-                print("Enter Phone Number: ")
-                val phoneNumber = scanner.next()
-                val addressDTO = AddressDTO(
-                    firstName,
-                    lastName,
-                    address,
-                    phoneNumber
-                )
-                addressService.addAddress(addressDTO)
-
-                println("Detail added successfully!. Detail:${addressDTO}")
+                handleAddAddress(scanner,addressService)
             }
             2 -> {
                 print("Enter Name Prefix: ")
@@ -52,4 +28,37 @@ fun main(){
             else -> println("Invalid option. Please choose a valid option.")
         }
     }
+}
+
+
+private fun printOptionMessage(){
+    println("\n1. Add Address Detail")
+    println("2. Search by Name Prefix")
+    println("3. Search by Number Prefix")
+    println("4. Exit")
+    print("Choose an option: ")
+}
+
+private fun handleAddAddress(scanner:Scanner,addressService:AddressService){
+    print("Enter First Name: ")
+    val firstName = scanner.next()
+
+    print("Enter Last Name: ")
+    val lastName = scanner.next()
+    scanner.nextLine()
+    print("Enter Address: ")
+    val address = scanner.nextLine()
+
+    print("Enter Phone Number: ")
+    val phoneNumber = scanner.next()
+    val addressDTO = AddressDTO(
+        firstName,
+        lastName,
+        address,
+        phoneNumber
+    )
+    addressService.addAddress(addressDTO)
+
+    println("Detail added successfully!. Detail:${addressDTO}")
+
 }
